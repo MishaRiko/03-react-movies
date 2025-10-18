cef4448ed3292e06899d9b4c6e88f99c3ef852cc
MishaRiko <bmishariko@gmail.com>
2025-10-18 20:35:50 +0300

    done all components

---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import type { FormEvent } from "react";
+
+import toast from "react-hot-toast";
+import css from "./SearchBar.module.css";
+
+interface SearchBarProps {
+  onSubmit: (query: string) => void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ onSubmit }) => {
+  const [query, setQuery] = useState("");
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (query.trim() === "") {
+      toast.error("Please enter your search query.");
+      return;
+    }
+
+    onSubmit(query.trim());
+    setQuery("");
+  };
+
+  return (
+    <header className={css.header}>
+      <div className={css.container}>
+        <a
+          className={css.link}
+          href="https://www.themoviedb.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by TMDB
+        </a>
+        <form className={css.form} onSubmit={handleSubmit}>
+          <input
+            className={css.input}
+            type="text"
+            name="query"
+            autoComplete="off"
+            placeholder="Search movies..."
+            autoFocus
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <button className={css.button} type="submit">
+            Search
+          </button>
+        </form>
+      </div>
+    </header>
+  );
+};
+
+export default SearchBar;
